c54b3acb1b19abd6aea014a78e41013bd8f92a6a
Schepin Dmitriy <bright1308@gmail.com>
2021-12-15 12:06:26 +0300

    add sex1

--- src/shchepin/examples/different/Person.java
+++ src/shchepin/examples/different/Person.java
@@ -3,6 +3,8 @@ package shchepin.examples.different;
 public class Person {
    private String name;
    private String age;
+   private String sex1;
+
 
     public String getName() {
         return name;
